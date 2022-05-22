030765ecec59ab1e79f0d8fd7fdc47f311917709
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-05-22 22:28:58 +0200

    feat: Add support for changing directory, including home and root directory

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use std::{env, io::{stdout, Write}};
+mod command;
+
+use std::{env::{self, set_current_dir}, io::{stdout, Write}};
 use std::process::Command as ProcessCommand;
+use std::path::Path;
+use home::home_dir;
 use text_io::read;
+use command::Command;
 
 fn print_working_directory() {
     let path = env::current_dir();
@@ -16,31 +21,48 @@ fn print_working_directory() {
     stdout().flush().unwrap();
 }
 
-struct Command<'a> {
-    command_name: &'a str,
-    arguments: Vec<&'a str>,
+fn is_cd_command(command: &str) -> bool {
+    command.eq("cd")
+}
+
+fn change_directory(path: &str) -> std::io::Result<()> {
+    let home_directory = home_dir().unwrap();
+    let home_as_path = home_directory.as_path();
+
+    let directory = match path {
+        "~" => home_as_path,
+        "" => Path::new("/"),
+        _ => Path::new(path)
+    };
+
+    set_current_dir(directory)
 }
 
 fn main() {
-    print_working_directory();
+    loop {
+        print_working_directory();
 
-    let user_input: String = read!("{}\n");
+        let input: String = read!("{}\n");
 
-    let split = user_input.split_whitespace();
-    let command_name = split.clone().next().unwrap();
-    let arguments = split.skip(1).collect::<Vec<&str>>();
+        let command = Command::new(&input);
 
-    let command = Command {
-        command_name,
-        arguments,
-    };
+        if is_cd_command(command.command_name) {
+            let path = command.arguments
+                .clone()
+                .into_iter()
+                .next()
+                .unwrap_or_default();
 
-    let result = {
-        ProcessCommand::new(command.command_name)
+            let _ = change_directory(path);
+
+            continue;
+        }
+
+        let result = ProcessCommand::new(command.command_name)
             .args(command.arguments)
             .output()
-            .expect("Failed to execute the command!")
-    };
+            .expect("Failed to execute the command!");
 
-    println!("{}", std::str::from_utf8(&result.stdout).unwrap());
+        println!("{}", std::str::from_utf8(&result.stdout).unwrap());
+    }
 }
